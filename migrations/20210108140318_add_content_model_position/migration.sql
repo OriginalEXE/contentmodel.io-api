256739af8a11a932a1c0e48e2cac8ba38fcfd767
Ante Sepic <antesepic@protonmail.com>
2021-01-24 22:24:55 +0100

    :fire: Remove migrations

--- migrations/20210108140318_add_content_model_position/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `position` to the `ContentModelVersion` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "ContentModelVersion" ADD COLUMN     "position" JSONB NOT NULL;
--- migrations/20210109105808_make_user_mandatory/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `userId` on table `ContentModel` required. The migration will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "ContentModel" ALTER COLUMN "userId" SET NOT NULL;
