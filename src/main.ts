93cbbc9722182cc4a2020caf9429d424d43f0764
Ante Sepic <antesepic@protonmail.com>
2021-01-31 21:12:14 +0100

    :wrench: Add support for local SSL

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions:
+      (process.env.LOCAL_SSL_KEY_PATH as string) &&
+      (process.env.LOCAL_SSL_CERT_PATH as string)
+        ? {
+            key: fs.readFileSync(
+              path.join(__dirname, process.env.LOCAL_SSL_KEY_PATH as string),
+            ),
+            cert: fs.readFileSync(
+              path.join(__dirname, process.env.LOCAL_SSL_CERT_PATH as string),
+            ),
+          }
+        : undefined,
+  });
 
   app.enableCors({
     credentials: true,
